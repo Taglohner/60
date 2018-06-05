13307796024de35c7fcc91c3786e807f83a2f030
Steven Taglohner <taglohner@me.com>
2018-06-05 21:54:24 +0100

    Initial Commit

---
+++ 60/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  60
+//
+//  Created by Steven Taglohner on 05/06/2018.
+//  Copyright © 2018 Steven Taglohner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
